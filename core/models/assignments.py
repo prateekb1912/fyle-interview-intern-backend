354b125e8904964f8ff4264eec8da8b23308d17d
Prateek Bhardwaj <54992024+prateekb1912@users.noreply.github.com>
2023-04-03 18:59:56 +0000

    Add view to list all assignments of a teacher

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -75,3 +75,7 @@ class Assignment(db.Model):
     @classmethod
     def get_assignments_by_student(cls, student_id):
         return cls.filter(cls.student_id == student_id).all()
+
+    @classmethod
+    def get_assignments_by_teacher(cls, teacher_id):
+        return cls.filter(cls.teacher_id == teacher_id).all()
