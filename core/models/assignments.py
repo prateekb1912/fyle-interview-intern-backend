08ff2f296b75ace4986b27cdd2d8d0926ed430de
Prateek Bhardwaj <54992024+prateekb1912@users.noreply.github.com>
2023-04-03 20:06:12 +0000

    Disallow assignment resubmissions by students

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -69,6 +69,7 @@ class Assignment(db.Model):
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(assignment.student_id == principal.student_id, 'This assignment belongs to some other student')
         assertions.assert_valid(assignment.content is not None, 'assignment with empty content cannot be submitted')
+        assertions.assert_valid(assignment.state == AssignmentStateEnum.DRAFT, 'only a draft assignment can be submitted')
 
         assignment.teacher_id = teacher_id
         assignment.state = AssignmentStateEnum.SUBMITTED
@@ -89,7 +90,9 @@ class Assignment(db.Model):
         assignment = Assignment.get_by_id(_id)
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(assignment.teacher_id == principal.teacher_id, 'This assignment was submitted to another teacher')
+        assertions.assert_valid(assignment.state == AssignmentStateEnum.DRAFT, 'Only a submitted assignment can be graded')
         assertions.assert_valid(GradeEnum.key_exists(assignment.grade), 'The assigned grade is invalid')
+        
 
         assignment.grade = grade
         assignment.status = AssignmentStateEnum.GRADED
