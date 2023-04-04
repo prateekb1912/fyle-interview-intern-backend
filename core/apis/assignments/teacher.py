d6b407a0d88cb7ce266a4e2532d4f5d74e54b300
Prateek Bhardwaj <54992024+prateekb1912@users.noreply.github.com>
2023-04-04 05:03:11 +0000

    resolve grade_assignment view error.

--- core/apis/assignments/teacher.py
+++ core/apis/assignments/teacher.py
@@ -3,8 +3,11 @@ from core import db
 from core.apis import decorators
 from core.apis.responses import APIResponse
 from core.models.assignments import Assignment
+import logging
 
 from .schema import AssignmentSchema, AssignmentGradingSchema
+
+logger = logging.Logger(__name__)
 
 teacher_assignments_resources = Blueprint('teacher_assignments_resources', __name__)
 
@@ -20,18 +23,24 @@ def list_assignments(p):
 @teacher_assignments_resources.route('/assignments/grade', methods=['POST'], strict_slashes=False)
 @decorators.auth_principal
 @decorators.accept_payload
-def grade_assignments(p, incoming_payload):
-    """Grade an assignment"""
-    grade_assignment_payload = AssignmentGradingSchema().load(incoming_payload)
+def grade_assignments(incoming_payload, p):
+    """Grade an assignment"""    
+    try:
+        grade_assignment_payload = AssignmentGradingSchema().load(incoming_payload)
+    except Exception as err:
+        print(err.messages)
+        return APIResponse.respond(data={'error': err.messages})
 
-    assignment = Assignment.grading(
+    print('Loaded payload:', grade_assignment_payload)  
+
+    graded_assignment = Assignment.grade_assignment(
         _id=grade_assignment_payload.id,
-        grade=grade_assignment_payload.grade,
+        _grade=grade_assignment_payload.grade,
         principal=p
     )
 
     db.session.commit()
     
-    graded_assignment_dump = AssignmentSchema().dump(assignment)
+    graded_assignment_dump = AssignmentSchema().dump(graded_assignment)
     
     return APIResponse.respond(data=graded_assignment_dump)
